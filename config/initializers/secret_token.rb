4fe762f2a06d2ca2310c5f75c93a666d24e80d82
Chris McCann <testflyjets@gmail.com>
2014-04-10 19:23:43 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MagicHangman::Application.config.secret_token = '14c365498f1d40a064e73ed23950922574de49df7db7b9ea1168523db4a1b9cb8d9912ab75085ee200ab7a609d9c5ff717183b9ba1cd76888a48329db2cfa0ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MagicHangman::Application.config.session_store :cookie_store, key: '_magic_hangman_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MagicHangman::Application.config.session_store :active_record_store
